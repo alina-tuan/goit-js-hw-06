68bd95bff72198bbbbf329a9de6335e087fb1bce
Alina <alina.bilchich@gmail.com>
2022-10-03 00:02:42 +0300

    2 task done

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,17 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsRef = document.querySelector("#ingredients");
+
+
+const elements = ingredients.map(ingredient => {
+    const itemRef = document.createElement("li");
+    itemRef.textContent = ingredient;
+    itemRef.classList.add('item');
+  
+    return itemRef;
+  });
+
+// console.log(elements);
+ingredientsRef.append(...elements);
