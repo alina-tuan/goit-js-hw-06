7fecf79d38fbb8e0233dfe61a058502330f24f21
Alina <alina.bilchich@gmail.com>
2022-10-03 12:46:39 +0300

    task 8 is fixed

--- js/task-08.js
+++ js/task-08.js
@@ -13,6 +13,6 @@ function handleSubmit(event) {
         return alert("All fields must be filled!");
     };
 
-    console.log(`Email: ${email.value}`, `Password: ${password.value}`);
+    console.log({"email": email.value, "password": password.value});
     event.currentTarget.reset();
 };
